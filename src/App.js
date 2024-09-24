63ca30634c8a9cd1dc11ce1b91331e8db6f31c7d
RakshitSrivastava-13 <rakshitsrivastava8291@gmail.com>
2024-09-24 22:29:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState } from 'react';
+import Login from './Login';
+import ImageGallery from './ImageGallery';
 
-function App() {
+const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = (username, password) => {
+    console.log('Username:', username);
+    console.log('Password:', password);
+    setLoggedIn(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{textAlign: "center"}}>Pet Animal: Dog</h1>
+      {!loggedIn ? (
+        <Login onLogin={handleLogin} />
+      ) : (
+        <ImageGallery />
+      )}
     </div>
   );
-}
+};
 
 export default App;
